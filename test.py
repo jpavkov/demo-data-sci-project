20226c1d01710aa303efd8475f268ae985e45ebf
jpavkov <jonathan.pavkov@gmail.com>
2022-08-01 10:32:17 -0400

    add place holder for Tina

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+from logging.handlers import TimedRotatingFileHandler
+from reprlib import aRepr
+from turtle import ycor
 import pandas as pd
 
-print("hello world!")
+print("HELLO WORLD!")
+
+def my_function():
+    pass
+
+def billys_function():
+    pass
+
+def tinas_function():
+    pass
+
+
+
